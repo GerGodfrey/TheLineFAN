95e4b43bb9936e1fc9a2525f5af075a50f8846b0
D9J9V <jimenezvdiego@gmail.com>
2023-06-16 18:54:49 -0600

    Create SearchBox component

---
+++ pages/SearchBox.jsx
@@ -0,0 +1,22 @@
+const SearchBox = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Función para manejar el cambio en el cuadro de búsqueda
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleChange}
+        placeholder="Buscar..."
+      />
+      <button type="submit">Buscar</button>
+    </div>
+  );
+};
+
+export default SearchBox;
