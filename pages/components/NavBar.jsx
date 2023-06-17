b4e1824b55c3c35b456d2400e10b870ddb23c1fe
D9J9V <jimenezvdiego@gmail.com>
2023-06-17 13:58:02 -0600

    Adding in-venue / robonomics

--- pages/components/NavBar.jsx
+++ pages/components/NavBar.jsx
@@ -1,52 +1,70 @@
-import {useState} from 'react';
+import React, { useState } from 'react';
 import SearchBox from './SearchBox';
 import Link from 'next/link';
 
-const NavBar =  () => {
-    return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-            <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Nosotros</a></li>
-                <li>
-                    <a>Proximamente</a>
-                    <ul className="p-2">
-                    <li><a>Español MX</a></li>
-                    <li><a>English US</a></li>
-                    </ul>
-                </li>
-                <li><a>Eventos</a></li>
-                </ul>
-            </div>
-            <Link href="/" legacyBehavior>
-                <a className="btn btn-ghost normal-case text-xl">Fan Lane</a>
-            </Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-                <li><a>Nosotros</a></li>
-                <li tabIndex={0}>
-                <details>
-                    <summary>Proximamente</summary>
-                    <ul className="p-2">
-                    <li><a>Español</a></li>
-                    <li><a>English</a></li>
-                    </ul>
-                </details>
-                </li>
-                <li><a>Eventos</a></li>
-            </ul>
-            </div>
-            <div className="navbar-end">
-            <a className="btn">Button</a>
-            </div>
-            <SearchBox />
+const NavBar = () => {
+  const [isInVenue, setIsInVenue] = useState(false);
+
+  const toggleInVenue = () => {
+  setIsInVenue(prevState => !prevState);
+
+  const url = "your-api-endpoint"; // Replace with your actual API endpoint
+
+  if (isInVenue) {
+    // Perform a GET request
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        // Process the response data
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle the error
+        console.error(error);
+      });
+  } else {
+    // Perform a POST request
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({}), // Replace with the request payload if needed
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Process the response data
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle the error
+        console.error(error);
+      });
+  }
+};
+
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        {/* Dropdown menu code */}
       </div>
-    )
-}
+      <Link href="/" legacyBehavior>
+        <a className="btn btn-ghost normal-case text-xl">Fan Lane</a>
+      </Link>
+      <div className="navbar-center hidden lg:flex">
+        {/* Menu code */}
+      </div>
+      <div className="navbar-end">
+        <a className="btn" onClick={toggleInVenue}>In-venue function</a>
+      </div>
+      <SearchBox />
+      <div>
+        {isInVenue ? <p>You are in the venue!</p> : null}
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
+
